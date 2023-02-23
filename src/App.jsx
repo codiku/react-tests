2dde67811336b9b6d7b78c5b832bb017056f9358
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-02-23 20:08:36 +0100

    starter

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,9 @@ import { Calculator } from "components/Calculator/Calculator";
 import s from "./App.module.css";
 import { useState } from "react";
 export function App() {
-  const [countCounterUpdates, setCounterUpdates] = useState(0);
   return (
     <div className={s.root}>
       <Calculator />
-      <Counter
-        initialValue={0}
-        onCounterUpdate={() => setCounterUpdates(countCounterUpdates + 1)}
-      />
-      <p>The counter has been updated {countCounterUpdates} times</p>
-      <RandomUser />
     </div>
   );
 }
