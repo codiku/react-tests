c7d247f5ff48681c8fccda8740620893a28a685b
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-02-24 20:34:41 +0100

    wip

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -2,6 +2,15 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import { Calculator } from "components/Calculator/Calculator";
 const { getValueA, getValueB, getResult, getOperator } = getCalculator();
 
+function getCalculator() {
+  return {
+    getValueA: () => screen.getByTestId("inputA").value,
+    getValueB: () => screen.getByTestId("inputB").value,
+    getOperator: () => screen.getByTestId("operator").value,
+    getResult: () => screen.getByTestId("result").textContent,
+  };
+}
+
 describe("<Calculator/>", () => {
   it("has 'Calculator' displayed somewhere", () => {
     render(<Calculator />);
@@ -32,7 +41,7 @@ describe("<Calculator/>", () => {
     expect(getResult()).toBe("120");
   });
 
-  it("calculates correctly when the user updates an input", async () => {
+  it("calculates correctly when the user updates an input", () => {
     render(<Calculator defaultA={12} defaultB={"10"} defaultOperator={"x"} />);
     fireEvent.change(screen.getByTestId("inputA"), { target: { value: "3" } });
     expect(getResult()).toBe("30");
@@ -44,33 +53,8 @@ describe("<Calculator/>", () => {
     expect(getResult()).toBe("0");
   });
 
-  it("concidere an empty input as 0", () => {
-    render(<Calculator defaultA={12} defaultB={"10"} defaultOperator={"x"} />);
-
-    fireEvent.change(screen.getByTestId("inputA"), { target: { value: "" } });
-    fireEvent.change(screen.getByTestId("inputB"), { target: { value: "" } });
-    expect(getResult()).toBe("0");
-  });
-
-  it("displays an error when the operator is invalid", () => {
-    render(<Calculator defaultA={12} defaultB={"10"} defaultOperator={"x"} />);
-    fireEvent.change(screen.getByTestId("operator"), {
-      target: { value: "Should diplay an error" },
-    });
-    expect(getResult()).toBe("No operator provided");
-  });
-
-  it("displays an error when we divide by 0 ", () => {
-    render(<Calculator defaultA={0} defaultB={"0"} defaultOperator={"/"} />);
+  it("displays an error when we divide by 0", () => {
+    render(<Calculator defaultA={1} defaultB={"0"} defaultOperator={"/"} />);
     expect(getResult()).toBe("You can't divide by 0");
   });
 });
-
-function getCalculator() {
-  return {
-    getValueA: () => screen.getByTestId("inputA").value,
-    getValueB: () => screen.getByTestId("inputB").value,
-    getOperator: () => screen.getByTestId("operator").value,
-    getResult: () => screen.getByTestId("result").textContent,
-  };
-}
