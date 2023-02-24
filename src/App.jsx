7783a5788456774cf0a37aa36e79cdd1b59cc9db
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-02-24 13:05:51 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { Counter } from "components/Counter/Counter";
-import { RandomUser } from "components/RandomUser/RandomUser";
 import { Calculator } from "components/Calculator/Calculator";
 import s from "./App.module.css";
-import { useState } from "react";
 export function App() {
   return (
     <div className={s.root}>
