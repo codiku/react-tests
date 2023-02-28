b6c1310ee645fa77f43d1a0374f0f73a9c4e73c4
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-02-28 19:17:39 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Calculator } from "components/Calculator/Calculator";
+import { RandomUser } from "components/RandomUser/RandomUser";
 import s from "./App.module.css";
 export function App() {
   return (
     <div className={s.root}>
-      <Calculator defaultA={5} defaultB={5} defaultOperator={"x"} />
+      {/* <Calculator defaultA={5} defaultB={5} defaultOperator={"x"} /> */}
+      <RandomUser />
     </div>
   );
 }
