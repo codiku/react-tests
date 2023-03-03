a61febbe2ae1fa59612e88b577dc5a247a7429d1
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-03-03 11:40:30 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { Calculator } from "components/Calculator";
 import s from "./App.module.css";
 export function App() {
-  return <div className={s.root}>Let's test :)</div>;
+  return (
+    <div className={s.root}>
+      <Calculator defaultB={"17.1"} defaultOperator={"-"} />
+    </div>
+  );
 }
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { sum, divide, multiply, substract } from "utils/math-functions";
+const OPERATORS = ["+", "-", "x", "/"];
+
+export function Calculator({ defaultA, defaultB, defaultOperator }) {
+  const [inputValueA, setInputValueA] = useState(
+    !defaultA || isNaN(defaultA) ? 0 : Number(defaultA)
+  );
+
+  const [inputValueB, setInputValueB] = useState(
+    !defaultB || isNaN(defaultB) ? 0 : Number(defaultB)
+  );
+
+  const [operator, setOperator] = useState(
+    OPERATORS.includes(defaultOperator) ? defaultOperator : "+"
+  );
+
+  const valueA = inputValueA || 0;
+  const valueB = inputValueB || 0;
+
+  function getResult() {
+    switch (operator) {
+      case "+":
+        return sum(valueA, valueB);
+      case "-":
+        return substract(valueA, valueB);
+      case "x":
+        return multiply(valueA, valueB);
+      case "/":
+        return divideSafely(valueA, valueB);
+      default:
+        return "No operator provided";
+    }
+  }
+
+  function divideSafely(a, b) {
+    try {
+      return divide(a, b);
+    } catch (err) {
+      return err.message;
+    }
+  }
+
+  const renderInputA = () => {
+    return (
+      <input
+        value={inputValueA}
+        type="number"
+        onChange={(e) =>
+          setInputValueA(
+            e.target.value ? Number.parseFloat(e.target.value) : ""
+          )
+        }
+      />
+    );
+  };
+
+  const renderInputB = () => {
+    return (
+      <input
+        value={inputValueB}
+        type="number"
+        onChange={(e) =>
+          setInputValueB(
+            e.target.value ? Number.parseFloat(e.target.value) : ""
+          )
+        }
+      />
+    );
+  };
+
+  const renderSelectBox = () => {
+    return (
+      <div>
+        <select
+          value={operator}
+          className="form-select"
+          onChange={(e) => setOperator(e.target.value)}
+        >
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="x">x</option>
+          <option value="/">/</option>
+        </select>
+      </div>
+    );
+  };
+  return (
+    <>
+      <h1 style={{ marginBottom: 40 }}>Calculator</h1>
+      {renderInputA()}
+      {renderSelectBox()}
+      {renderInputB()}
+      <h2 style={{ marginTop: 20 }}>Result</h2>
+      {getResult()}
+    </>
+  );
+}
